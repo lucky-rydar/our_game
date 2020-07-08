4b01a21ef6ac05e0ffa37e70f761cee066916a7d
Misha <mihasyaterminator@gmail.com>
2020-07-08 15:15:37 +0300

    Defined base classes on branch Clent

---
+++ client/Client.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <SFML/Network.hpp>
+
+class Client
+{
+public:
+
+private:
+
+};
+
---
+++ client/Game.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+
+class Game
+{
+public:
+
+private:
+
+};
+
