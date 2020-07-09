4639430c36c34a585d5d2f8dff98df6951783d19
Misha <mihasyaterminator@gmail.com>
2020-07-09 16:52:09 +0300

    Added draw and update funcs to InterfaceManager

--- client/Game/InterfaceManager.cpp
+++ client/Game/InterfaceManager.cpp
@@ -2,12 +2,39 @@
 
 InterfaceManager::InterfaceManager()
 {
-	this->gameMenu = new GameMenu;
-	this->mainMenu = new MainMenu;
+	gameMenu = new GameMenu;
+	mainMenu = new MainMenu;
+
+	window = new RenderWindow(VideoMode(1280, 720), "our_game");
 }
 
 InterfaceManager::~InterfaceManager()
 {
 	delete this->gameMenu;
 	delete this->mainMenu;
+	delete this->window;
+}
+
+void InterfaceManager::update()
+{
+	// TO DO here call update func of gameMenu and mainMenu 
+}
+
+void InterfaceManager::draw()
+{
+	if (window->isOpen())
+	{
+		Event eve;
+		while (window->pollEvent(eve))
+		{
+			if (eve.type == Event::Closed)
+				window->close();
+		}
+
+		window->clear(Color(0, 0, 0, 0));
+
+		// TO DO drawing gamMenu and mainMenu
+
+		window->display();
+	}
 }
--- client/Game/InterfaceManager.h
+++ client/Game/InterfaceManager.h
@@ -1,19 +1,23 @@
 #pragma once
 #include "InterfaceManager/GameMenu.h"
 #include "InterfaceManager/MainMenu.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 class InterfaceManager
 {
 public:
-
 	InterfaceManager();
-
 	~InterfaceManager();
 
+	void update();
+	void draw();
+
 private:
-
 	MainMenu *mainMenu;
-
 	GameMenu *gameMenu;
+	RenderWindow *window;
+	
 };
 
