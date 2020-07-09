1ed459ae9c696efc66ea902cc75c87d472b444c1
Misha <mihasyaterminator@gmail.com>
2020-07-09 14:29:29 +0300

    Added functionality to Config class and added config.cfg file

--- client/Game/Config.h
+++ client/Game/Config.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
 class Config
 {
+public:
+	Config();
+	~Config();
+	void loadConfig(string fileName = "config.cfg");
+	string getParam(string key);
+
+private:
+	fstream configFile;
+	map<string, string> *params;
 };
 
