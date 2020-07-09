1c5c6c04312dec76d8935aec8c2db0eace649252
Владислав Подрезенко <pakgpcoans@gmail.com>
2020-07-09 14:47:54 +0300

    Memory allocation

--- client/Game.cpp
+++ client/Game.cpp
@@ -2,10 +2,14 @@
 
 Game::Game()
 {
-
+	this->client = new Client;
+	this->cfg = new Config;
+	this->imanager = new InterfaceManager;
 }
 
 Game::~Game()
 {
-
+	delete this->client;
+	delete this->cfg;
+	delete this->imanager;
 }
