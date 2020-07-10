3c409c6435985b3908b37cfbcf576af4a7de934d
Misha <mihasyaterminator@gmail.com>
2020-07-10 09:31:28 +0300

    Defined base funcs in GameMenu class

--- client/Game/InterfaceManager/GameMenu.cpp
+++ client/Game/InterfaceManager/GameMenu.cpp
@@ -1,1 +1,17 @@
 #include "GameMenu.h"
+
+GameMenu::GameMenu()
+{
+}
+
+GameMenu::~GameMenu()
+{
+}
+
+void GameMenu::draw(RenderWindow* win)
+{
+}
+
+void GameMenu::update(Client* client)
+{
+}
--- client/Game/InterfaceManager/GameMenu.h
+++ client/Game/InterfaceManager/GameMenu.h
@@ -7,7 +7,11 @@ using namespace sf;
 class GameMenu
 {
 public:
+	GameMenu();
+	~GameMenu();
 
+	void draw(RenderWindow *win);
+	void update(Client *client);
 private:
 
 };
