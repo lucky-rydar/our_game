42298c57285a1c7dd415d5fddfe95c1ed4edf8f7
Misha <mihasyaterminator@gmail.com>
2020-07-10 09:36:01 +0300

    Allocating memory in GameMenu

--- client/Game/InterfaceManager/GameMenu.cpp
+++ client/Game/InterfaceManager/GameMenu.cpp
@@ -2,10 +2,16 @@
 
 GameMenu::GameMenu()
 {
+	gameInterface = new GameInterface;
+	gameScene = new GameScene;
+	pauseMenu = new PauseMenu;
 }
 
 GameMenu::~GameMenu()
 {
+	delete gameInterface;
+	delete gameScene;
+	delete pauseMenu;
 }
 
 void GameMenu::draw(RenderWindow* win)
--- client/Game/InterfaceManager/GameMenu.h
+++ client/Game/InterfaceManager/GameMenu.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "../Client.h"
 #include<SFML/Graphics.hpp>
+#include "GameMenu/GameInterface.h"
+#include "GameMenu/GameScene.h"
+#include "GameMenu/PauseMenu.h"
 
 using namespace sf;
 
@@ -13,6 +16,8 @@ public:
 	void draw(RenderWindow *win);
 	void update(Client *client);
 private:
-
+	GameInterface* gameInterface;
+	GameScene* gameScene;
+	PauseMenu* pauseMenu;
 };
 
