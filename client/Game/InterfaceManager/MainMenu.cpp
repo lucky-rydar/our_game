1d0377f4db3da7f82e7d7b8e4fdae831c1dc8383
Владислав Подрезенко <pakgpcoans@gmail.com>
2020-07-10 11:26:59 +0300

    Interface elements drawing

--- client/Game/InterfaceManager/MainMenu.cpp
+++ client/Game/InterfaceManager/MainMenu.cpp
@@ -19,7 +19,9 @@ MainMenu::~MainMenu()
 
 void MainMenu::draw(RenderWindow* win)
 {
-
+	this->button->draw(*win);
+	this->label->draw(*win);
+	this->lineEdit->draw(*win);
 }
 
 void MainMenu::update(Client* client, CurrentIMMenu& curMenu)
