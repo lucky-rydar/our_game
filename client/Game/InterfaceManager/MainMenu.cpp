e093ded60d9a278828a3b1e97a173b57729961eb
Владислав Подрезенко <pakgpcoans@gmail.com>
2020-07-11 15:38:21 +0300

    Added sprite button im menu

--- client/Game/InterfaceManager/MainMenu.cpp
+++ client/Game/InterfaceManager/MainMenu.cpp
@@ -7,7 +7,13 @@ MainMenu::MainMenu(sf::Vector2f WindowSize, Event *eve, RenderWindow* win, Curre
 	font = new Font;
 	font->loadFromFile(std::string("Resources\\Fonts\\lilita_one_rus.ttf"));
 
-	this->button = new Button(font, sf::String("Play"), WindowSize.x / 2, WindowSize.y / 2, 150.f, 30.f, sf::Color::Red, win);
+	/*this->button = new Button(font, sf::String("Play"), WindowSize.x / 2, WindowSize.y / 2, 150.f, 30.f, sf::Color::Red, win);
+	this->button->setOnButtonFunc([curMenu]() {*curMenu = CurrentIMMenu::Game; });*/
+
+	sf::Texture texture;
+	texture.loadFromFile("Resources\\Sprites\\Interface\\Button 1.png");
+
+	this->button = new SpriteButton(font, sf::String("Play"), WindowSize.x / 2, WindowSize.y / 2, 1.f, 1.f, texture, win);
 	this->button->setOnButtonFunc([curMenu]() {*curMenu = CurrentIMMenu::Game; });
 
 	this->label = new Label(font, sf::String("our_game"), WindowSize.x / 2, WindowSize.y, sf::Color::White);
