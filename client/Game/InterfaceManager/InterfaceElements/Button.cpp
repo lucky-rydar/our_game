409a380a070511ef202ccbd6e6dbd987d5a38280
Владислав Подрезенко <pakgpcoans@gmail.com>
2020-07-11 14:01:40 +0300

    Sprite button class added

--- client/Game/InterfaceManager/InterfaceElements/Button.cpp
+++ client/Game/InterfaceManager/InterfaceElements/Button.cpp
@@ -61,3 +61,44 @@ bool Button::isOnButton(Vector2i pos)
 {
 	return this->getGlobalBounds().contains((float)pos.x, (float)pos.y);
 }
+
+
+SpriteButton::SpriteButton(sf::Font* font, sf::String str, float x, float y, float scalex, float scaley, sf::Texture texture, sf::RenderWindow* wnd)
+{
+	this->wnd = wnd;
+	this->texture = texture;
+
+	this->setScale(scalex, scaley);
+	this->setTexture(this->texture);
+
+	sf::FloatRect bounds;
+	const sf::Vector2f box(
+		this->getTexture()->getSize().x * this->getScale().x,
+		this->getTexture()->getSize().y * this->getScale().y);
+
+	this->setPosition(x, y);
+
+	this->ButtonText = new sf::Text(str, *font);
+
+	bounds = this->ButtonText->getLocalBounds();
+
+	this->ButtonText->setOrigin((bounds.width - box.x) / 2 + bounds.left, (bounds.height - box.y) / 2 + bounds.top);
+
+	this->ButtonText->setPosition(x, y);
+}
+
+SpriteButton::~SpriteButton()
+{
+	delete this->ButtonText;
+}
+
+void SpriteButton::draw()
+{
+	wnd->draw(*this);
+	wnd->draw(*this->ButtonText);
+}
+
+void SpriteButton::update()
+{
+
+}
--- client/Game/InterfaceManager/InterfaceElements/Button.h
+++ client/Game/InterfaceManager/InterfaceElements/Button.h
@@ -23,3 +23,22 @@ private:
 	RenderWindow *wnd;
 };
 
+class Collider
+{
+public:
+private:
+};
+
+class SpriteButton : public sf::Sprite
+{
+public:
+	SpriteButton(sf::Font* font, sf::String str, float x, float y, float scalex, float scaley, sf::Texture texture, sf::RenderWindow* wnd);
+	~SpriteButton();
+	void draw();
+	void update();
+
+	sf::Text* ButtonText;
+private:
+	RenderWindow* wnd;
+	sf::Texture texture;
+};
