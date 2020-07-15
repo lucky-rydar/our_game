e564ef0e46e68453b566a2583dbb05552ec623a8
Misha <mihasyaterminator@gmail.com>
2020-07-15 13:13:30 +0300

    Added base code to Player class

--- server/CLI/GameData/Player.cpp
+++ server/CLI/GameData/Player.cpp
@@ -1,1 +1,8 @@
 #include "Player.h"
+
+Player::Player(string email, string login, string password)
+{
+	this->email = email;
+	this->login = login;
+	this->password = password;
+}
--- server/CLI/GameData/Player.h
+++ server/CLI/GameData/Player.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Player
 {
+public:
+	Player(string email, string login, string password);
+
+	string email;
+	string login;
+	string password;
+private:
+	
 };
 
